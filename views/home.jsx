f74f98577bf87c5f983508a713aaaf04150792e9
Dakota Greenlee <dg2120032@gmail.com>
2023-01-09 19:18:55 -0800

    Jazzed up form page

--- views/home.jsx
+++ views/home.jsx
@@ -3,19 +3,21 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-              <a href="/places">
-  <button className="btn btn-primary">Places Page</button>
-</a>
-
-          </main>
-      </Def>
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="" alt="Chia Fruit Shake" />
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
     )
-  }
-  
+}
 
 module.exports = home
+
 
 
