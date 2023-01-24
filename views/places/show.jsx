e63031439f89c44b4771d126fecc77782a5476e9
Dakota Greenlee <dg2120032@gmail.com>
2023-01-23 19:39:26 -0800

    comments on show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,9 +2,29 @@ const React = require('react')
 const Def = require('../default')
 
 function show(data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant! >:(' : 'Rave! :)'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <stong>- {c.author}</stong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
+                <div className="row">
                 <h1>{data.place.name}</h1>
                 <img src={data.place.pic}/>
                 <h3>Located in {data.place.city}, {data.place.state}</h3>
@@ -13,8 +33,10 @@ function show(data) {
                 <h2>Description</h2>
                 <h3>{data.place.showEstablished()}</h3>
                 <h4>Serving {data.place.cuisines}</h4>
+                </div>
+                <hr />
                 <h2>Comments</h2>
-                <h4>No Comments</h4>
+                {comments}
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                     Edit
                 </a>     
