6da7400aa7959cdd6abf021fdaef11f110a10691
Dakota Greenlee <dg2120032@gmail.com>
2023-01-25 19:16:36 -0800

    ratings calculation

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,23 @@ function show(data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        if (data.place.comments.length) {
+            let sumRatings = data.place.comments.reduce((tot, c) => {
+                return tot + c.stars
+            }, 0)
+            let averageRating = sumRatings / data.place.comments.length
+            rating = (
+                <h3>
+                    {Math.round(averageRating)} stars
+                </h3>
+            )
+        }
         comments = data.place.comments.map(c => {
             return (
                 <div className="border">
@@ -19,6 +35,7 @@ function show(data) {
                     <h4>Rating: {c.stars}</h4>
                 </div>
             )
+
         })
     }
     return (
@@ -29,7 +46,8 @@ function show(data) {
                 <img src={data.place.pic}/>
                 <h3>Located in {data.place.city}, {data.place.state}</h3>
                 <h2>Rating</h2>
-                <h4>Not Rated</h4>
+                {rating}
+                <hr />
                 <h2>Description</h2>
                 <h3>{data.place.showEstablished()}</h3>
                 <h4>Serving {data.place.cuisines}</h4>
